25566170625772d8bd9b2b8503922e52d192b2e3
Zane.Zhao <nearile@163.com>
2015-02-03 00:08:29 +0800

    learn 2 & 3

---
+++ urllib2_test09.py
@@ -0,0 +1,26 @@
+from urllib2 import Request, urlopen, URLError, HTTPError
+
+req = Request('http://www.baidu.com')
+  
+try:  
+  
+    response = urlopen(req)  
+  
+except URLError, e:  
+
+    if hasattr(e, 'code'):  
+  
+        print 'The server couldn\'t fulfill the request.'  
+  
+        print 'Error code: ', e.code  
+
+    elif hasattr(e, 'reason'):  
+  
+        print 'We failed to reach a server.'  
+  
+        print 'Reason: ', e.reason  
+  
+  
+else:  
+    print 'No exception was raised.'  
+    # everything is fine  
